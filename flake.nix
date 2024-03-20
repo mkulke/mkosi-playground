524e51cca61d20e1e6e4e48f3ff846335bc9285d
Magnus Kulke <magnuskulke@microsoft.com>
2024-03-20 18:14:21 +0100

    wip: bump to nix unstable
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "mkosi environment";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
