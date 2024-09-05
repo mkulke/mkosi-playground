e7fdb9140b07774d723b01ec88e886677a30900b
Magnus Kulke <magnuskulke@microsoft.com>
2024-09-05 11:35:11 +0200

    improved launch util
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- launch-vm/go.mod
+++ launch-vm/go.mod
@@ -3,26 +3,26 @@ module github.com/mkulke/mkosi-playground/launch-vm
 go 1.20
 
 require (
-	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.9.2
-	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.5.1
-	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4 v4.2.1
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.14.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.7.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v5 v5.7.0
 	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2 v2.2.1
 	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v1.2.0
-	github.com/urfave/cli/v2 v2.27.1
+	github.com/urfave/cli/v2 v2.27.4
 )
 
 require (
-	github.com/Azure/azure-sdk-for-go/sdk/internal v1.5.2 // indirect
-	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.1 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
-	github.com/google/uuid v1.5.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.10.0 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 )
--- launch-vm/main.go
+++ launch-vm/main.go
@@ -8,19 +8,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/urfave/cli/v2"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v5"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
-)
-
-const (
-	location = "eastus"
 )
 
 var (
@@ -29,7 +26,12 @@ var (
 	imageID           string
 	subscriptionID    string
 	keep              bool
+	secureBoot        bool
 	sshPublicKeyPath  string
+	location          string
+	instanceSize      string
+	confidential      bool
+	vmName            string
 )
 
 var (
@@ -87,6 +89,41 @@ func main() {
 				Required:    true,
 			},
 			&cli.BoolFlag{
+				Name:        "secure-boot",
+				Aliases:     []string{"b"},
+				Usage:       "enable secure boot for the virtual machine",
+				Destination: &secureBoot,
+			},
+			&cli.BoolFlag{
+				Name:        "confidential",
+				Aliases:     []string{"c"},
+				Usage:       "enable confidential computing for the virtual machine",
+				Destination: &confidential,
+			},
+			&cli.StringFlag{
+				Name:        "location",
+				Aliases:     []string{"l"},
+				Usage:       "location of the virtual machine",
+				EnvVars:     []string{"LOCATION"},
+				Destination: &location,
+				Value:       "westeurope",
+			},
+			&cli.StringFlag{
+				Name:        "name",
+				Usage:       "name of the virtual machine",
+				EnvVars:     []string{"VM_NAME"},
+				Destination: &vmName,
+				Value:       "test-vm",
+			},
+			&cli.StringFlag{
+				Name:        "size",
+				Aliases:     []string{"z"},
+				Usage:       "instance size of the virtual machine",
+				EnvVars:     []string{"INSTANCE_SIZE"},
+				Destination: &instanceSize,
+				DefaultText: "Standard_DC2as_v5",
+			},
+			&cli.BoolFlag{
 				Name:        "keep",
 				Aliases:     []string{"k"},
 				Usage:       "do not delete the resources after creating them",
@@ -98,10 +135,11 @@ func main() {
 				Usage:       "ssh public key path",
 				EnvVars:     []string{"SSH_PUBLIC_KEY"},
 				Destination: &sshPublicKeyPath,
+				DefaultText: "~/.ssh/id_ed25519.pub",
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
-			resources, err := createVM("test-vm")
+			resources, err := createVM()
 			if err != nil {
 				return err
 			}
@@ -117,7 +155,67 @@ func main() {
 	}
 }
 
-func createVM(name string) (*VMResources, error) {
+func buildNetworkConfig(nicName string) *armcompute.VirtualMachineNetworkInterfaceConfiguration {
+	ipConfig := armcompute.VirtualMachineNetworkInterfaceIPConfiguration{
+		Name: to.Ptr("ip-config"),
+		Properties: &armcompute.VirtualMachineNetworkInterfaceIPConfigurationProperties{
+			Subnet: &armcompute.SubResource{
+				ID: to.Ptr(subnetID),
+			},
+		},
+	}
+
+	config := armcompute.VirtualMachineNetworkInterfaceConfiguration{
+		Name: to.Ptr(nicName),
+		Properties: &armcompute.VirtualMachineNetworkInterfaceConfigurationProperties{
+			DeleteOption:     to.Ptr(armcompute.DeleteOptionsDelete),
+			IPConfigurations: []*armcompute.VirtualMachineNetworkInterfaceIPConfiguration{&ipConfig},
+		},
+	}
+	return &config
+}
+
+func buildSSHConfig(userName string) (*armcompute.SSHConfiguration, error) {
+	config := armcompute.SSHConfiguration{
+		PublicKeys: []*armcompute.SSHPublicKey{},
+	}
+	path := sshPublicKeyPath
+	if path == "" {
+		homedir, err := os.UserHomeDir()
+		if err != nil {
+			return nil, err
+		}
+		path = homedir + "/.ssh/id_ed25519.pub"
+	}
+	var bytes []byte
+	_, err := os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err = os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	remotePath := fmt.Sprintf("/home/%s/.ssh/authorized_keys", userName)
+	key := armcompute.SSHPublicKey{
+		Path:    to.Ptr(remotePath),
+		KeyData: to.Ptr(string(bytes)),
+	}
+	config.PublicKeys = append(config.PublicKeys, &key)
+	return &config, nil
+}
+
+func buildImageRef(id string) *armcompute.ImageReference {
+	ref := armcompute.ImageReference{}
+	if strings.HasPrefix(id, "/CommunityGalleries") {
+		ref.CommunityGalleryImageID = to.Ptr(id)
+	} else {
+		ref.ID = to.Ptr(id)
+	}
+	return &ref
+}
+
+func createVM() (*VMResources, error) {
 	conn, err := connectionAzure()
 	if err != nil {
 		return nil, err
@@ -138,25 +236,19 @@ func createVM(name string) (*VMResources, error) {
 	disksClient = computeClientFactory.NewDisksClient()
 
 	log.Println("start creating virtual machine...")
+	nicName := vmName + "-nic"
+	diskName := vmName + "-disk"
 
-	nicName := name + "-nic"
-	nic, err := createNetWorkInterface(ctx, subnetID, nicName)
+	virtualMachine, err := createVirtualMachine(ctx, nicName, diskName, vmName)
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("created network interface: %s", *nic.ID)
-
-	diskName := name + "-disk"
-	virtualMachine, err := createVirtualMachine(ctx, *nic.ID, diskName, name)
-	if err != nil {
-		return nil, err
-	}
-	log.Printf("created network virtual machine: %s", *virtualMachine.ID)
+	log.Printf("created virtual machine: %s", *virtualMachine.ID)
 
 	log.Println("virtual machine created successfully")
 
 	return &VMResources{
-		vmName:   name,
+		vmName:   vmName,
 		nicName:  nicName,
 		diskName: diskName,
 	}, nil
@@ -197,39 +289,7 @@ func connectionAzure() (azcore.TokenCredential, error) {
 	return cred, nil
 }
 
-func createNetWorkInterface(ctx context.Context, subnetID string, name string) (*armnetwork.Interface, error) {
-	parameters := armnetwork.Interface{
-		Location: to.Ptr(location),
-		Properties: &armnetwork.InterfacePropertiesFormat{
-			IPConfigurations: []*armnetwork.InterfaceIPConfiguration{
-				{
-					Name: to.Ptr("ipConfig"),
-					Properties: &armnetwork.InterfaceIPConfigurationPropertiesFormat{
-						PrivateIPAllocationMethod: to.Ptr(armnetwork.IPAllocationMethodDynamic),
-						Subnet: &armnetwork.Subnet{
-							ID: to.Ptr(subnetID),
-						},
-					},
-				},
-			},
-		},
-	}
-
-	pollerResponse, err := interfacesClient.BeginCreateOrUpdate(ctx, resourceGroupName, name, parameters, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := pollerResponse.PollUntilDone(ctx, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return &resp.Interface, err
-}
-
 func deleteNetWorkInterface(ctx context.Context, name string) error {
-
 	pollerResponse, err := interfacesClient.BeginDelete(ctx, resourceGroupName, name, nil)
 	if err != nil {
 		return err
@@ -243,32 +303,39 @@ func deleteNetWorkInterface(ctx context.Context, name string) error {
 	return nil
 }
 
-func createVirtualMachine(ctx context.Context, networkInterfaceID string, diskName string, name string) (*armcompute.VirtualMachine, error) {
-	//require ssh key for authentication on linux
-	if sshPublicKeyPath == "" {
-		homedir, err := os.UserHomeDir()
-		if err != nil {
-			return nil, err
-		}
-		sshPublicKeyPath = homedir + "/.ssh/id_rsa.pub"
-	}
-	var sshBytes []byte
-	_, err := os.Stat(sshPublicKeyPath)
-	if err != nil {
-		return nil, err
-	}
-	sshBytes, err = os.ReadFile(sshPublicKeyPath)
-	if err != nil {
-		return nil, err
-	}
+func createVirtualMachine(ctx context.Context, nicName string, diskName string, name string) (*armcompute.VirtualMachine, error) {
 	userName := "azureuser"
 
+	sshConfig, err := buildSSHConfig(userName)
+	if err != nil {
+		return nil, err
+	}
+
+	networkConfig := buildNetworkConfig(nicName)
+
 	securityProfile := &armcompute.SecurityProfile{
-		SecurityType: to.Ptr(armcompute.SecurityTypesConfidentialVM),
+		SecurityType: to.Ptr(armcompute.SecurityTypesTrustedLaunch),
 		UefiSettings: &armcompute.UefiSettings{
-			SecureBootEnabled: to.Ptr(false),
+			SecureBootEnabled: to.Ptr(secureBoot),
 			VTpmEnabled:       to.Ptr(true),
 		},
+	}
+
+	osDiskParams := &armcompute.OSDisk{
+		Name:         to.Ptr(diskName),
+		CreateOption: to.Ptr(armcompute.DiskCreateOptionTypesFromImage),
+		Caching:      to.Ptr(armcompute.CachingTypesReadWrite),
+		DeleteOption: to.Ptr(armcompute.DiskDeleteOptionTypesDelete),
+		ManagedDisk: &armcompute.ManagedDiskParameters{
+			StorageAccountType: to.Ptr(armcompute.StorageAccountTypesStandardLRS),
+		},
+	}
+
+	if confidential {
+		securityProfile.SecurityType = to.Ptr(armcompute.SecurityTypesConfidentialVM)
+		osDiskParams.ManagedDisk.SecurityProfile = &armcompute.VMDiskSecurityProfile{
+			SecurityEncryptionType: to.Ptr(armcompute.SecurityEncryptionTypesNonPersistedTPM),
+		}
 	}
 
 	parameters := armcompute.VirtualMachine{
@@ -278,43 +345,18 @@ func createVirtualMachine(ctx context.Context, networkInterfaceID string, diskNa
 		},
 		Properties: &armcompute.VirtualMachineProperties{
 			StorageProfile: &armcompute.StorageProfile{
-				ImageReference: &armcompute.ImageReference{
-					//CommunityGalleryImageID: to.Ptr(imageID),
-					ID: to.Ptr(imageID),
-				},
-				OSDisk: &armcompute.OSDisk{
-					Name:         to.Ptr(diskName),
-					CreateOption: to.Ptr(armcompute.DiskCreateOptionTypesFromImage),
-					Caching:      to.Ptr(armcompute.CachingTypesReadWrite),
-					DeleteOption: to.Ptr(armcompute.DiskDeleteOptionTypesDelete),
-					ManagedDisk: &armcompute.ManagedDiskParameters{
-						StorageAccountType: to.Ptr(armcompute.StorageAccountTypesStandardLRS), // OSDisk type Standard/Premium HDD/SSD
-						SecurityProfile: &armcompute.VMDiskSecurityProfile{
-							SecurityEncryptionType: to.Ptr(armcompute.SecurityEncryptionTypesVMGuestStateOnly),
-						},
-					},
-					DiskSizeGB: to.Ptr[int32](20), // default 127G
-				},
+				ImageReference: buildImageRef(imageID),
+				OSDisk:         osDiskParams,
 			},
 			HardwareProfile: &armcompute.HardwareProfile{
-				VMSize: to.Ptr(armcompute.VirtualMachineSizeTypes("Standard_DC2as_v5")),
+				VMSize: to.Ptr(armcompute.VirtualMachineSizeTypes(instanceSize)),
 			},
-			OSProfile: &armcompute.OSProfile{ //
+			OSProfile: &armcompute.OSProfile{
 				ComputerName:  to.Ptr(name),
 				AdminUsername: to.Ptr(userName),
-				// 	AdminPassword: to.Ptr("Password01!@#"),
-				//require ssh key for authentication on linux
 				LinuxConfiguration: &armcompute.LinuxConfiguration{
 					DisablePasswordAuthentication: to.Ptr(true),
-					SSH: &armcompute.SSHConfiguration{
-						// PublicKeys: []*armcompute.SSHPublicKey{},
-						PublicKeys: []*armcompute.SSHPublicKey{
-							{
-								Path:    to.Ptr(fmt.Sprintf("/home/%s/.ssh/authorized_keys", userName)),
-								KeyData: to.Ptr(string(sshBytes)),
-							},
-						},
-					},
+					SSH:                           sshConfig,
 				},
 			},
 			SecurityProfile: securityProfile,
@@ -324,14 +366,8 @@ func createVirtualMachine(ctx context.Context, networkInterfaceID string, diskNa
 				},
 			},
 			NetworkProfile: &armcompute.NetworkProfile{
-				NetworkInterfaces: []*armcompute.NetworkInterfaceReference{
-					{
-						ID: to.Ptr(networkInterfaceID),
-						Properties: &armcompute.NetworkInterfaceReferenceProperties{
-							DeleteOption: to.Ptr(armcompute.DeleteOptionsDelete),
-						},
-					},
-				},
+				NetworkAPIVersion:              to.Ptr(armcompute.NetworkAPIVersionTwoThousandTwenty1101),
+				NetworkInterfaceConfigurations: []*armcompute.VirtualMachineNetworkInterfaceConfiguration{networkConfig},
 			},
 		},
 	}
